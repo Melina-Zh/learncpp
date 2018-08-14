3355088f82b33a3ac28d6a1efdb9c8276d720031
Melina-Zh <zhynzm_85@126.com>
2018-08-14 19:12:53 +0800

    lambdaprc

---
+++ lambdaprc/lambdaprc/lambdaprc.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lambdaprc
+//
+//  Created by Melina on 2018/8/14.
+//  Copyright © 2018年 Melina. All rights reserved.
+//
+//lambda
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    [](){cout << "Hello, World!\n";}();
+    return 0;
+}
